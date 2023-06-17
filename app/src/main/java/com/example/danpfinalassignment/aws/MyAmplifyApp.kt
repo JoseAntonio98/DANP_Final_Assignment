3806b479d95b79d2802d5f57aa905eef4f2f86ed
walterHC <Fabiola2050>
2023-06-17 17:28:33 -0500

    Creacion de la clase para iniciar la conexion con amplify v2

--- app/src/main/java/com/example/danpfinalassignment/aws/MyAmplifyApp.kt
+++ app/src/main/java/com/example/danpfinalassignment/aws/MyAmplifyApp.kt
@@ -3,6 +3,7 @@ package com.example.danpfinalassignment.aws
 import android.app.Application
 import android.util.Log
 import com.amplifyframework.AmplifyException
+import com.amplifyframework.auth.cognito.AWSCognitoAuthPlugin
 import com.amplifyframework.core.Amplify
 
 class MyAmplifyApp : Application() {
@@ -10,8 +11,12 @@ class MyAmplifyApp : Application() {
         super.onCreate()
 
         try {
+            Amplify.addPlugin(AWSCognitoAuthPlugin())
+            Log.i("MyAmplifyApp", "Initialized Cognito")
+
             Amplify.configure(applicationContext)
             Log.i("MyAmplifyApp", "Initialized Amplify")
+
         } catch (error: AmplifyException) {
             Log.e("MyAmplifyApp", "Could not initialize Amplify", error)
         }
--- app/src/main/java/com/example/danpfinalassignment/ui/screens/MainActivity.kt
+++ app/src/main/java/com/example/danpfinalassignment/ui/screens/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.danpfinalassignment.ui.screens
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,6 +9,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
+import com.amplifyframework.core.Amplify
 import com.example.danpfinalassignment.ui.theme.DANPFinalAssignmentTheme
 import com.example.danpfinalassignment.ui.theme.TertiaryColor
 import com.example.danpfinalassignment.util.navigation.NavigationAppHost
@@ -22,6 +24,11 @@ class MainActivity : ComponentActivity() {
                     // color = MaterialTheme.colorScheme.background
                     color = TertiaryColor
                 ) {
+                    Amplify.Auth.fetchAuthSession(
+                        { Log.i("AmplifyQuickstart", "Auth session = $it") },
+                        { error -> Log.e("AmplifyQuickstart", "Failed to fetch auth session", error) }
+                    )
+
                     val navController = rememberNavController()
                     NavigationAppHost(navController = navController)
                 }
