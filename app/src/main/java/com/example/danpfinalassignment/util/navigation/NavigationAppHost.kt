623f96140d279a01212dc798758be63e3b4eff7a
Jose Antonio <sawm5798@gmail.com>
2023-06-21 02:18:17 -0500

    Changed start destination to home

--- app/src/main/java/com/example/danpfinalassignment/util/navigation/NavigationAppHost.kt
+++ app/src/main/java/com/example/danpfinalassignment/util/navigation/NavigationAppHost.kt
@@ -7,24 +7,23 @@ import androidx.navigation.compose.composable
 import com.example.danpfinalassignment.ui.screens.EmergencyContactsScreen
 import com.example.danpfinalassignment.ui.screens.HistoryScreen
 import com.example.danpfinalassignment.ui.screens.HomeScreen
-import com.example.danpfinalassignment.ui.screens.LoginScreen
-import com.example.danpfinalassignment.ui.screens.RegisterScreen
-import com.example.danpfinalassignment.ui.screens.RegisterVerificationScreen
 import com.example.danpfinalassignment.ui.screens.SettingsScreen
 
 @Composable
 fun NavigationAppHost(navController: NavHostController) {
-    NavHost(navController = navController, startDestination = Destination.Login.route) {
+    NavHost(navController = navController, startDestination = Destination.Home.route) {
         composable(Destination.EmergencyContacts.route) { EmergencyContactsScreen(navController = navController) }
         composable(Destination.History.route) { HistoryScreen(navController = navController) }
         composable(Destination.Home.route) { HomeScreen(navController = navController) }
-        composable(Destination.Login.route) { LoginScreen(navController = navController) }
-        composable(Destination.Register.route) { RegisterScreen(navController = navController) }
+        // composable(Destination.Login.route) { LoginScreen(navController = navController) }
+        // composable(Destination.Register.route) { RegisterScreen(navController = navController) }
+        /*
         composable(Destination.RegisterVerification.route) {
             RegisterVerificationScreen(
                 navController = navController
             )
         }
+        */
         composable(Destination.Settings.route) { SettingsScreen(navController = navController) }
     }
 }
